a1582a3e47f146ab02f43f52d810fde4eaa31609
thee-falcon <omar.makran.10@gmail.com>
2025-10-30 20:19:47 +0100

    register-view working good

--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -1,7 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import '../firebase_options.dart';
 
 class RegisterView extends StatefulWidget {
   const RegisterView({super.key});
@@ -13,17 +11,13 @@ class RegisterView extends StatefulWidget {
 class _RegisterViewState extends State<RegisterView> {
   late final TextEditingController _email;
   late final TextEditingController _password;
-  late final Future<FirebaseApp> _initialization;
   bool _obscureText = true;
 
   @override
   void initState() {
     _email = TextEditingController();
     _password = TextEditingController();
-    _initialization = Firebase.initializeApp(
-      options: DefaultFirebaseOptions.currentPlatform,
-    );
-    // Rebuild the widget when the password text changes to update the icon visibility.
+
     _password.addListener(() {
       setState(() {});
     });
@@ -41,92 +35,82 @@ class _RegisterViewState extends State<RegisterView> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text('Register'),
       ),
-      body: FutureBuilder(
-        future: _initialization,
-        builder: (context, snapshot) {
-          switch (snapshot.connectionState) {
-            case ConnectionState.done:
-              return Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    TextField(
-                      controller: _email,
-                      autocorrect: false,
-                      keyboardType: TextInputType.emailAddress,
-                      enableSuggestions: false,
-                      decoration: const InputDecoration(
-                        border: OutlineInputBorder(),
-                        hintText: 'Email',
-                      ),
-                    ),
-                    const SizedBox(height: 16.0),
-                    TextField(
-                      controller: _password,
-                      autocorrect: false,
-                      obscureText: _obscureText,
-                      enableSuggestions: false,
-                      decoration: InputDecoration(
-                        border: const OutlineInputBorder(),
-                        hintText: 'Password',
-                        // Show the icon only if the password field is not empty.
-                        suffixIcon: _password.text.isNotEmpty
-                            ? IconButton(
-                          icon: Icon(
-                            // Change the icon based on the obscureText state.
-                            _obscureText
-                                ? Icons.visibility_off
-                                : Icons.visibility,
-                          ),
-                          onPressed: () {
-                            // Toggle the password's visibility on press.
-                            setState(() {
-                              _obscureText = !_obscureText;
-                            });
-                          },
-                        )
-                            : null, // Otherwise, show no icon.
-                      ),
-                    ),
-                    const SizedBox(height: 16.0),
-                    TextButton(
-                      onPressed: () async {
-                        try {
-                          // Create a new user with the provided email and password.
-                          final userCredential =
-                          await FirebaseAuth.instance.createUserWithEmailAndPassword(
-                            email: _email.text,
-                            password: _password.text,
-                          );
-                          // Consider showing a success message to the user.
-                          print('Successfully registered: ${userCredential.user?.uid}');
-                        } on FirebaseAuthException catch (e) {
-                          if (e.code == 'weak-password') {
-                            print('The password provided is too weak.');
-                          } else if (e.code == 'email-already-in-use') {
-                            print('The account already exists for that email.');
-                          } else if (e.code == 'invalid-email') {
-                            print('The email address is badly formatted.');
-                          } else {
-                            print('Error registering: ${e.message}');
-                            print(e.code);
-                          }
-                        }
-                      },
-                      child: const Text('Register'),
-                    ),
-                  ],
-                ),
-              );
-            default:
-              return const Center(child: CircularProgressIndicator());
-          }
-        },
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            TextField(
+              controller: _email,
+              autocorrect: false,
+              keyboardType: TextInputType.emailAddress,
+              enableSuggestions: false,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Email',
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            TextField(
+              controller: _password,
+              autocorrect: false,
+              obscureText: _obscureText,
+              enableSuggestions: false,
+              decoration: InputDecoration(
+                border: const OutlineInputBorder(),
+                hintText: 'Password',
+                suffixIcon: _password.text.isNotEmpty
+                    ? IconButton(
+                        icon: Icon(
+                          _obscureText
+                              ? Icons.visibility_off
+                              : Icons.visibility,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            _obscureText = !_obscureText;
+                          });
+                        },
+                      )
+                    : null,
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            TextButton(
+              onPressed: () async {
+                try {
+                  await FirebaseAuth.instance.createUserWithEmailAndPassword(
+                    email: _email.text,
+                    password: _password.text,
+                  );
+                } on FirebaseAuthException catch (e) {
+                  if (e.code == 'weak-password') {
+                    print('The password provided is too weak.');
+                  } else if (e.code == 'email-already-in-use') {
+                    print('The account already exists for that email.');
+                  } else if (e.code == 'invalid-email') {
+                    print('The email address is badly formatted.');
+                  } else {
+                    print('Error registering: ${e.message}');
+                  }
+                }
+              },
+              child: const Text('Register'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pushNamedAndRemoveUntil(
+                  '/login/',
+                  (route) => false,
+                );
+              },
+              child: const Text('Already have an account? Login here!'),
+            )
+          ],
+        ),
       ),
     );
   }
