e87ed030ece41b16f01f192f18f4d599892e9de4
thee-falcon <omar.makran.10@gmail.com>
2025-10-31 20:38:38 +0100

    Customize the Error alert for iOS and Android

--- lib/utilities/show_error_dialog.dart
+++ lib/utilities/show_error_dialog.dart
@@ -1,21 +1,41 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 Future<void> showErrorDialog(BuildContext context, String text) {
-  return showDialog(
-    context: context,
-    builder: (context) {
-      return AlertDialog(
+  if (Platform.isIOS) {
+    return showCupertinoDialog<void>(
+      context: context,
+      builder: (context) => CupertinoAlertDialog(
         title: const Text('An error occurred'),
         content: Text(text),
         actions: [
-          TextButton(
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
+          CupertinoDialogAction(
+            isDefaultAction: true,
+            onPressed: () => Navigator.of(context).pop(),
             child: const Text('OK'),
           ),
         ],
-      );
-    },
-  );
+      ),
+    );
+  } else {
+    return showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('An error occurred'),
+          content: Text(text),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('OK'),
+            ),
+          ],
+        );
+      },
+    );
+  }
 }
