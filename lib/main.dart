566dca11a48c33aaacd937c168d79074e28c0f6e
thee-falcon <omar.makran.10@gmail.com>
2025-11-05 06:23:25 +0100

    clean up the main

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,9 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:mynote/services/auth/auth_service.dart';
 import 'package:mynote/views/login_view.dart';
 import 'package:mynote/views/notes_view.dart';
 import 'package:mynote/views/register_view.dart';
 import 'package:mynote/views/verify_email_view.dart';
-import 'dart:developer' as devtools show log;
-
-import 'firebase_options.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -27,47 +23,32 @@ class MyApp extends StatelessWidget {
       ),
       home: const HomePage(),
       routes: {
-        '/login/' : (context) => const LoginView(),
-        '/register/' : (context) => const RegisterView(),
-        '/verify-email/' : (context) => const VerifyEmailView(),
-        '/notes/' : (context) => const NotesView(),
+        '/login/': (context) => const LoginView(),
+        '/register/': (context) => const RegisterView(),
+        '/verify-email/': (context) => const VerifyEmailView(),
+        '/notes/': (context) => const NotesView(),
       },
       debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  late final Future<FirebaseApp> _initialization;
-
-  @override
-  void initState() {
-    _initialization = Firebase.initializeApp(
-      options: DefaultFirebaseOptions.currentPlatform,
-    );
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-      future: _initialization,
+      future: AuthService.firebase().initializer(),
       builder: (context, snapshot) {
         switch (snapshot.connectionState) {
           case ConnectionState.done:
             return StreamBuilder(
-              stream: FirebaseAuth.instance.authStateChanges(),
+              stream: AuthService.firebase().authStateChanges,
               builder: (context, snapshot) {
                 if (snapshot.hasData) {
-                  final user = FirebaseAuth.instance.currentUser;
-                  if (user?.emailVerified ?? false) {
+                  final user = AuthService.firebase().currentUser;
+                  if (user?.isEmailVerified ?? false) {
                     return const NotesView();
                   } else {
                     return const VerifyEmailView();
